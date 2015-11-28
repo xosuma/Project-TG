6e33ba45597fa50a062e9a915f49e82431d21b1a
kikicool <jaeyoep@gmail.com>
2015-11-27 22:50:20 -0600

    Admin flag

--- models/User.js
+++ models/User.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var UserSchema = new mongoose.Schema({
   name: String,
   email: String,
-  address: String
+  address: String,
+  admin: Boolean
 })
 
 module.exports = mongoose.model('User', UserSchema);
